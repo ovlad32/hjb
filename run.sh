58e85d64f71f5eab7c4a18effcff944686f24d03
ovlad32 <vladymyr.orlovskiy@rokittechua.com>
2021-10-07 13:05:01 +0300

    first

--- run.sh
+++
@@ -1,9 +0,0 @@
-dbfile="hjb.db3"
-rm "${dbfile}";
-if [ ! -f "${dbfile}" ]; then
-    go run . -mode=init
-    go run . -mode=indexing -baseTag="mt-base"
-    go run . -mode=discovery -baseTag="mt-base" -lakeTag="cra-lake" -anchorUniqueness=1 -anchorMinCount=3 -anchorMaxCount=4
-    go run . -mode=aggregation -baseTag="mt-base" -baseRowMinPct=0
-fi
-
